e85b07ae21ac696e814e90e6b69015daeacb0a8b
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-04-19 13:27:38 +0200

    Delete pages_app directory

--- pages_app/db.py
+++
@@ -1,8 +0,0 @@
-from supabase import create_client, Client
-import streamlit as st
-
-@st.cache_resource
-def get_supabase() -> Client:
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
